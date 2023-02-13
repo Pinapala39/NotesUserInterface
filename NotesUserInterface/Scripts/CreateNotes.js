c80d5389526b201cf388300976752205506fbe23
saikr <saikr@SAIKRISHNA>
2023-02-13 21:56:10 +0100

    Add project files.

---
+++ NotesUserInterface/Scripts/CreateNotes.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#myNote').submit(function (event) {
+        event.preventDefault();
+
+        var id = Math.floor(Math.random() * 1000);
+        var title = $('#title').val();
+        var desc = $('#desc').val();
+        var content = $('#content').val();
+
+        $.post('https://localhost:7224/api/CreateNotes', { id: id, title: title, desc: desc, content: content })
+            .done(function (response) {
+                console.log(response);
+            })
+            
+    });
+});
